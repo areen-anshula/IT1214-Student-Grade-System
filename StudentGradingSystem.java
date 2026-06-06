0c3e4755da95e487ab51d75cac1458970bbc175f
Areen Anshula <areenanshula008@gmail.com>
2026-06-06 08:57:07 +0530

    creating class Student

---
+++ StudentGradingSystem.java
@@ -0,0 +1,16 @@
+class Student {
+    private String studentID;
+    private String studentName;
+    private int marks;
+    public Student(String studentID, String studentName, int marks) {
+        this.studentID=studentID;
+        this.studentName=studentName;
+        this.marks=marks;
+    }
+}
+
+public class  StudentGradingSystem {
+    public static void main(String[] args) {
+        
+    }
+}
